c812f2ffbf6b97f98ced422c30b5e736f587e466
LyreLilith <103064593+LyreLilith@users.noreply.github.com>
2023-02-18 13:17:32 -0800

    wip monster inplmentation

--- CrimsonCodeHackathon/Monster.hpp
+++ CrimsonCodeHackathon/Monster.hpp
@@ -1,87 +1,22 @@
 #include <string>
-
+#include "Movement.hpp"
 using namespace std;
 
 class Monster{
 
 	
 public:
-	Monster();
+	Monster(void);
 
 	Monster(string name, struct AbilityScores &scores, struct actions &actions  );
 
 
-	typedef struct savingThrow
-	{
-		int str;
-		int dex;
-		int con;
-		int intel;
-		int wis;
-		int cha;
-	}SavingThrows;
-
-	typedef struct abilityScore
-	{
-		int str;
-		int dex;
-		int con;
-		int intel;
-		int wis;
-		int cha;
-		SavingThrows saves;
-	}AbilityScores;
-
-
-	typedef struct sense
-	{
-		int passive;
-		int dark;
-		int blind;
-		int truesight;
-		int tremor;
-	}Senses;
-
-	typedef struct damage 
-	// This refers to how different types of damage effect the monster, not how much damage the creature itself can do
-	// may want to display as strings, but characters work too if they correspond to effects (v=vulnerable, r=resistant, etc)
-	{
-		char bludgeon;
-		char pierce;
-		char slash;
-		char acid;
-		char poison;
-		char fire;
-		char cold;
-		char force;
-		char lightning;
-		char thunder;
-		char necro;
-		char psychic;
-		char radiant;
-	}Damage;
-
-	typedef struct stats
-	{
-		int armorClass;
-		int health;
-		int combatRating;
-		struct Senses;
-	}Stats;
-
-	typedef struct actions // feels like it may be a good idea to just use strings to write them out rather than use variables
-	{
-		string name;
-		int hitMod;
-		string damageDie; // referring to amount and type of dice to roll for damage
-		string description; // ability description. may or may not remove since tends to be long. might shorten instead?
-	}Actions;
-
-	
-
 private:
-	Stats monsterStats;
+	Movement monsterMove;
+	AbilityScores monsterScores;
+	SavingThrows monsterSavingThrows;
 	Actions monsterActions;
 	string name;
+	string descripton
 	
 };
