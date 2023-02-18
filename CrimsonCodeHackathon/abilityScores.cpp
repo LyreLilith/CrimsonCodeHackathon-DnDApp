f4ab0515f3340b1890d8079d5f5068bd5dd6e4f2
OlySamRock <olysamrock@gmail.com>
2023-02-18 15:26:57 -0800

    introduced stats.hpp, did something with abilityScores.hpp

--- CrimsonCodeHackathon/abilityScores.cpp
+++ CrimsonCodeHackathon/abilityScores.cpp
@@ -1,1 +1,43 @@
 #include "abilityScores.hpp"
+
+
+AbilityScores::AbilityScores()
+{
+	dex = 0;
+	con = 0;
+	str = 0;
+	intel = 0;
+	wis = 0;
+	cha = 0;
+}
+
+AbilityScores::AbilityScores(int dex, int con, int str, int intel, int wis, int cha)
+{
+	this->cha = cha;
+	this->con = con;
+	this->str = str;
+	this->intel = intel;
+	this->wis = wis;
+	this->dex = dex;
+}
+
+int AbilityScores::getDex(void)
+{
+	return dex;
+}
+
+int AbilityScores::getCon(void)
+{
+	return con;
+}
+
+int AbilityScores::getStr(void)
+{
+	return str;
+}
+
+int AbilityScores::getDex(void)
+{
+	return cha;
+}
+
