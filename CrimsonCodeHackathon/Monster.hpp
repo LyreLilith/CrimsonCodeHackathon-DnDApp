2611c504207c314f15195024ba0a581ad9831c97
OlySamRock <olysamrock@gmail.com>
2023-02-18 23:39:38 -0800

    defining monster functions

--- CrimsonCodeHackathon/Monster.hpp
+++ CrimsonCodeHackathon/Monster.hpp
@@ -3,6 +3,7 @@
 #include "abilityScores.hpp"
 #include "senses.hpp"
 #include "savingThrows.hpp"
+#include "actions.hpp"
 #include "stats.hpp"
 
 using namespace std;
@@ -13,18 +14,14 @@ class Monster{
 public:
 	Monster(void);
 
-	Monster(string name, struct AbilityScores &scores, struct Actions &actions, struct Stats &stats);
+	Monster(string &name, struct AbilityScores &scores, struct Stats &stats, struct Actions &actions, string &description);
 
+	string getName(void);
+	AbilityScores getScores(void);
+	Stats getStats(void);
+	Actions getActions(void);
+	string getDescription(void);
 
-	typedef struct actions // feels like it may be a good idea to just use strings to write them out rather than use variables
-	{
-		string name;
-		int hitMod;
-		string damageDie; // referring to amount and type of dice to roll for damage
-		string description; // ability description. may or may not remove since tends to be long. might shorten instead?
-	}Actions;
-
-	
 
 private:
 SavingThrows monsterSave;
--- CrimsonCodeHackathon/monster.cpp
+++ CrimsonCodeHackathon/monster.cpp
@@ -1,6 +1,33 @@
 #include "Monster.hpp"
+#include <string>
 using namespace std;
 
-Monster::Monster(string name, Stats monsterStats) {
-
+Monster::Monster(void)
+{
+	std::string name;
+	AbilityScores scores();
+	Stats stats();
+	Actions actions();
 }
+
+Monster::Monster(string nameIN, AbilityScores &scoresIN, Stats &statsIN, Actions &actionsIN)
+{
+	name = nameIN;
+	scores = scoresIN;
+	stats = statsIN;
+	actions = actionsIN;
+}
+
+string Monster::getName(void)
+{
+	return name;
+}
+
+AbilityScores Monster::getScores(void)
+{
+	return scores;
+}
+
+Stats getStats(void);
+Actions getActions(void);
+string getDescription(void);
