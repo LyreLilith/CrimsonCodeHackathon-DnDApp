1d38dbd5ee463f3a3b65f7a29b2790281dc222bc
OlySamRock <olysamrock@gmail.com>
2023-02-18 23:42:24 -0800

    moving read function

--- CrimsonCodeHackathon/functions.cpp
+++ CrimsonCodeHackathon/functions.cpp
@@ -4,19 +4,6 @@
 #include <fstream>
 using namespace std;
 
-void readCSV(FILE* infile)
-{
-    string line;
-    ifstream monsters;
-    monsters.open("monsters.csv");
 
-    if (!monsters.is_open()) {
-        perror("Error open");
-        exit(EXIT_FAILURE);
-    }
-    while (getline(monsters, line)) {
-        cout << line << endl;
-    }
-}
 
 // make into function
