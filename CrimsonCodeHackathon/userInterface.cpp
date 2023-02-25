34005129429b918820853a41e35d0e4a0f242b49
OlySamRock <olysamrock@gmail.com>
2023-02-24 16:14:11 -0800

    idk

--- CrimsonCodeHackathon/userInterface.cpp
+++ CrimsonCodeHackathon/userInterface.cpp
@@ -1,3 +1,25 @@
 #include "UserInterface.hpp"
 
-UserInterface::UserInterface(MonsterList mList){
+UserInterface::UserInterface(MonsterList mList) {
+	mlData = mList;
+}
+
+Monster UserInterface::monsterSelectior(void)
+{
+	string monsterName;
+	std::cout << "Please enter a the name of a monster to search" << std::endl;
+	std::cin >> monsterName;
+	return mlData.search(monsterName);
+}
+
+void UserInterface::displayMonster(Monster mon) {
+	Stats mStat = mon.getStats();
+	Senses mSenses = mon.getSenses();
+	std::cout << "NAME: " << mon.getName() << std::endl;
+	std::cout <<mStat << std::endl;
+	std::cout << mSenses << std::endl;
+	std::cout << "Description: "<< mon.getDescription() << std::endl;
+	
+}
+
+	
